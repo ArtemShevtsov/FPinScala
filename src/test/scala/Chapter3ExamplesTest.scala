7bc41d93bf4cf166066200ab09494601febbcfc6
Artem Shevtsov <ext-artem.shevtsov@here.com>
2020-02-06 18:06:22 +0200

    start Tree class

--- src/test/scala/Chapter3ExamplesTest.scala
+++ src/test/scala/Chapter3ExamplesTest.scala
@@ -1,4 +1,4 @@
-import book.examples.chapter3.{Cons, List, Nil}
+import book.examples.chapter3.{Branch, Leaf, Cons, List, Nil, Tree}
 
 object Chapter3ExamplesTest {
   private def x = List(1,2,3,4,5) match {
@@ -20,6 +20,11 @@ object Chapter3ExamplesTest {
     println("REVERSE of List(1,12,3, 4): " + List.reverse(List(1,12,3, 4)))
     println("Append 5 to List(1,12,3, 4): " + List.appendFolding(List(1,12,3, 4), List(5)))
 
+    val tree: Tree[String] = Branch(Branch(Leaf("a"), Leaf("b")), Branch(Leaf("c"), Leaf("d")))
+    println(Tree.size(tree))
+
+    val tree1: Tree[String] = Branch(Branch(Leaf("a"), Leaf("b")), Branch(Branch(Leaf("c"), Leaf("d")), Branch(Leaf("e"), Branch(Leaf("f"), Leaf("g")))))
+    println(Tree.depth(tree1))
   }
 
   def initTest(): Unit = {
